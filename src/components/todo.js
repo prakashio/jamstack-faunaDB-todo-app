222f12bd21586f776601d0f167961648fd8ac1da
Prakash Kumar <prakash.kumar@active.ai>
2020-04-21 20:02:56 +0530

    feat(todo): add delete feature

--- src/components/todo.js
+++ src/components/todo.js
@@ -13,6 +13,14 @@ const Todo = ({ todo, reloadTodos }) => {
       .then(reloadTodos);
   };
 
+  const handleDelete = () => {
+    axios
+      .post("/api/delete-todo", {
+        id: todo._id,
+      })
+      .then(reloadTodos);
+  };
+
   return (
     <>
       <lable htmlFor={`todo-toggle-${todo._id}`} className={styles.label}>
@@ -28,6 +36,16 @@ const Todo = ({ todo, reloadTodos }) => {
       <p className={`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
       </p>
+
+      <label
+        htmlFor={`todo-delete-${todo._id}`}
+        className={styles.label}
+      ></label>
+      <button onClick={handleDelete} className={styles.delete}>
+        <span role="img" aria-label="delete" title="delete this todo">
+          ❌
+        </span>
+      </button>
     </>
   );
 };
